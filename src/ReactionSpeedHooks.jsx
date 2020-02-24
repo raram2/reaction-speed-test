d25bc729bcf82565a9a16886b3c5d9d31606ade7
raram2 <gkfka1189@gmail.com>
2020-02-24 13:24:10 +0900

    Update: useRef, useEffect

--- src/ReactionSpeedHooks.jsx
+++ src/ReactionSpeedHooks.jsx
@@ -1,34 +1,39 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 
 const ReactionSpeed = () => {
   const [sign, setSign] = useState('waiting');
   const [message, setMessage] = useState('클릭하면 시작됩니다.');
   const [record, setRecord] = useState(0);
   const [records, setRecords] = useState([]);
-  let timeout, startTime, endTime; // states not for rendering
+  // Not for DOM update
+  const timeout = useRef(null);
+  const startTime = useRef(null);
+  const endTime = useRef(null);
+  // Methods
   const handleClick = () => {
     if (sign === 'waiting') {
-      // when red light, then to yellow light, then to green light
+      // When red light, then to yellow light, then to green light
       setSign('ready');
       setMessage('초록색이 되면 클릭하세요.');
-      timeout = setTimeout(() => {
+      timeout.current = setTimeout(() => {
         setSign('now');
         setMessage('지금 클릭!');
-        startTime = new Date();
+        startTime.current = new Date();
+        console.log(startTime.current);
       }, Math.round(Math.random() * 1000 + 2000));
     } else if (sign === 'ready') {
-      // assign penalty
-      clearTimeout(timeout);
+      // Assign penalty
+      clearTimeout(timeout.current);
       setSign('waiting');
       setMessage('너무 성급하시군요. 클릭하면 다시 시작됩니다.');
       setRecord(1000);
-      setRecords(prevRecords => prevRecords.concat(1000));
+      setRecords(prevRecords => [...prevRecords, 1000]);
     } else if (sign === 'now') {
-      endTime = new Date();
+      endTime.current = new Date();
       setSign('waiting');
       setMessage('클릭하면 시작됩니다.');
-      setRecord((endTime - startTime).toFixed(0));
-      setRecords(prevRecords => prevRecords.concat((endTime - startTime).toFixed(0)));
+      setRecord((endTime.current - startTime.current).toFixed(0));
+      setRecords(prevRecords => [...prevRecords, (endTime.current - startTime.current).toFixed(0)]);
     }
   };
   const getAverageSpeed = () => {
@@ -47,13 +52,16 @@ const ReactionSpeed = () => {
   };
   const renderReactionSpeed = () => {
     return record === 0 ? null : (
-      <div>{record === 1000 ? <span className='penalty'>페널티</span> : record}ms</div>
+      <div>
+        {record === 1000 ? <span className='penalty'>페널티 : </span> : '현재 반응속도 : '}
+        {record}ms
+      </div>
     );
   };
   const renderAverageSpeed = () => {
     return records.length === 0 ? null : <div>평균 반응속도: {getAverageSpeed()}ms</div>;
   };
-  useEffect(() => finishOrNot(), [records]);
+  useEffect(() => finishOrNot());
   return (
     <>
       <div id='screen' className={sign} onClick={handleClick}>
